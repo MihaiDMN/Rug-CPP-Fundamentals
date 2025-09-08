2623a8bf9a3c4aeb53cbd8bd7515ac50b3e03635
DRS05 <daan307rosies@gmail.com>
2025-09-08 20:07:56 +0000

    Fix issues assignment1

--- Assignment1/exercise7/argument.cc
+++ Assignment1/exercise7/argument.cc
@@ -2,10 +2,10 @@
 #include <string>
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	cout << "Hello " << (argc == 1 ? "World"
-						: (argc ==2 ? argv[1]
+						: (argc == 2 ? argv[1]
 						: argv[stoul(argv[1])]))
 		 << '\n';
 }
